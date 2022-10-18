63ca7d5b36fa88b811a7906c6b83a67a59bc8325
vivianokose <vivianokose@gmail.com>
2022-10-18 09:39:42 -0700

    practice.c

--- topman_assignment/practice.c
+++ topman_assignment/practice.c
@@ -3,5 +3,27 @@
 int main() 
 {
 	printf("Hello, World\n");
+	
+	int x = 50;
+	int y; //declaration
+	y = 10; //initialization
+
+	printf("%s World\n", "Hello");
+
+	printf("The value of x is %d\n", x);
+
+	printf("X: %d, Y: %d\n", x, y);
+
+	printf("Give me a radius: ");
+	int radius;
+	scanf("%d", &radius); //address-of-operator (pointer)
+
+	printf("You choose the value %d\n", radius);
+	
+	char name [20]; //char array of 20 characters. '\0' takes one spot
+	printf("what is your name?: ");
+	scanf("%19s", name);
+	
+	printf("Your name: %s\n", name);
 	return 0;
 }
